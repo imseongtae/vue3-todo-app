b5e4169e4046184cc40386d190519a468fb4e900
imseongtae <dev.haemil@gmail.com>
2020-12-06 03:53:40 +0900

    feat: Add TodoItem component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<li v-for="(item, index) in todoItems" :key="index" class="todo-item">
+			<p>
+				{{ item.content }}
+				<button @click="remove(index)">❌</button>
+			</p>
+		</li>
+	</div>
+</template>
+
+<script>
+export default {
+	inheritAttrs: false,
+	props: {
+		todoItems: {
+			type: Array,
+			required: true,
+		},
+	},
+	methods: {
+		remove(index) {
+			this.$emit('remove', index);
+		},
+	},
+};
+</script>
