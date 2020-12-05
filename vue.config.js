b7f54d0f8c7be2eaae5ad5994a914a81971dc32d
imseongtae <dev.haemil@gmail.com>
2020-12-06 03:10:27 +0900

    chore: Add _ui.scss

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,14 @@ module.exports = {
 	devServer: {
 		overlay: false,
 	},
+
+	css: {
+		loaderOptions: {
+			sass: {
+				additionalData: `
+          @import "@/scss/_ui.scss";
+        `,
+			},
+		},
+	},
 };
