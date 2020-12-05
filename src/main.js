5d788dbbd2159888f4022406619805397bee7231
imseongtae <dev.haemil@gmail.com>
2020-12-06 03:53:02 +0900

    feat: Add todo store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 import router from './router';
 import App from './App.vue';
+import todoStroe from './store/todo';
 
 import 'normalize-css';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).provide('todo', todoStroe()).mount('#app');
---
+++ src/store/todo.js
@@ -0,0 +1,25 @@
+import { reactive, readonly } from 'vue';
+
+const createStore = () => {
+	const state = reactive({
+		list: [],
+	});
+
+	const push = content => {
+		state.list.push({
+			content,
+		});
+	};
+
+	const remove = index => {
+		state.list.splice(index, 1);
+	};
+
+	return {
+		state: readonly(state),
+		push,
+		remove,
+	};
+};
+
+export default createStore;
